971c4fbd2306f8b40b1f062a314557212f3b3190
dass90 <3053034939@qq.com>
2025-07-01 15:04:09 +0800

    [feat] 心愿界面具体信息跳转

--- src/renderer/views/Wish.vue
+++ src/renderer/views/Wish.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="wish" class="vertical">
+        <!-- petmate好感度信息展示 -->
         <div class="petmate-affection-overview dark-white border-view">
             <div class="avator">
                 <Avator/>
@@ -21,6 +22,8 @@
                 </div>
             </div>
         </div>
+
+        <!-- 完成心愿和正在进行的心愿的总览 -->
         <div class="wish-completion-overview medium-white border-view">
             <div class="wish-completed light-white dashboard">
                 <div class="wish-statics"><label>完成心愿</label></div>
@@ -39,6 +42,8 @@
                 </div>
             </div>
         </div>
+        
+        <!-- 所有的心愿信息，可以查看 -->
         <div class="wish-container dark-white border-view">
             <div class="wish-container-header medium-white">
                 <div class="header-label dark-white">
@@ -46,11 +51,51 @@
                 </div>
             </div>
             <div class="to-do-wishes medium-white wish-item">
-                <n-infinite-scroll style="height: 100%">
+                <n-infinite-scroll v-if="check===false" style="height: 100%">
                     <div v-for="i in 15" :key="i" class="item" style="height: 33%;">
-                        <WishItem/>
+                        <WishItem @click="check = !check"/>
                     </div>
                 </n-infinite-scroll>
+                <div v-else class="wish-completion-info">
+                    <!-- 心愿得基本信息 -->
+                    <div class="wish-completion-basic-info">
+                        <!--返回按键-->
+                        <div class="return-button">
+                            <n-button @click="check = !check">
+                                <img src="../../assets/image/return.png" style="width: 20px;"/>
+                            </n-button>
+                        </div>
+                        <img src="../../assets/image/wish.png" style="width: 50px; height: 50px;"/>
+                        <div class="wish-completion-baisc-info-item">
+                            <div class="wish-name" style="font-size: 16px;">
+                                夏日清凉小确幸
+                            </div>
+                            <div class="wish-description">
+                                <label style="font-size: 13px;">战斗双！</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div><label>目前进度</label></div>
+                    <!-- 完成心愿得要求和玩家目前的进度 -->
+                    <div class="wish-completion-requirements">
+                        <div class="wish-completion-requirements-activity wish-completion-requirements-item">
+                
+                        </div>
+
+                        <div class="wish-completion-requriements-consume-item wish-completion-requirements-item">
+                            <div v-for="item in itemProgress">
+                                <img :src="item.url" style="width: 20px;"/>
+                                {{ item.name }} ({{ item.giveNum }} / {{ item.requirementNum }})
+                                <img v-if="item.status === 'completed'" src="../../assets/image/right.png" style="width: 15px;"/>
+                                <img v-else src="../../assets/image/wrong.png" style="width: 15px;"/>
+                            </div>
+                        </div>
+                        
+                        <div class="wish-completion-requirements-chat wish-completion-requirements-item">
+                            123
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -59,12 +104,41 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import Avator from '../components/Avator.vue'
-import WishItem from '../components/WishItem.vue'
+import WishItem from '../components/wish/WishItem.vue'
+import WishCompletionInfo from '../components/wish/WishCompletionInfo.vue'
 
 const petmateName = "Dass"
 const affenctionLevel = ref(1)
 const currentAffenctionExp = ref(60)
 const nextAffectionExp = "/100"
+const check = ref(false)
+
+let itemProgress = ref([
+    {
+        "id": 1,
+        "name": "汉堡",
+        "url": "../../assets/image/wish.png",
+        "requirementNum": 2,
+        "giveNum": 1,
+        "status": "noCompleted"
+    },
+    {
+        "id": 1,
+        "name": "汉堡",
+        "url": "../../assets/image/wish.png",
+        "requirementNum": 2,
+        "giveNum": 1,
+        "status": "completed"
+    },
+    {
+        "id": 1,
+        "name": "汉堡",
+        "url": "../../assets/image/wish.png",
+        "requirementNum": 2,
+        "giveNum": 1,
+        "status": "noCompleted"
+    }
+]) 
 
 </script>
 
@@ -166,6 +240,48 @@ label {
     font-weight: bold;
     padding: 10px;
     margin: 10px;
+}
+
+.wish-completion-info {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    row-gap: 5px;
+    width: 100%;
+    height: 100%;
+    .wish-completion-basic-info {
+        display: flex;
+        .wish-completion-baisc-info-item {
+            display: flex;
+            flex-direction: column;
+            column-gap: 5px;
+            padding-top: 10px;
+        }
+    }
+    .wish-completion-requirements {
+        display: flex;
+        column-gap: 5px;
+        background-color: $bg-white-300;
+        border-radius: 5px;
+        justify-content: center;
+        height: 100%;
+    }
+}
+
+.wish-completion-requirements-item {
+    background-color: $bg-white-100;
+    border-radius: 5px;
+    margin: 5px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 5px;
+    width: 100%;
+    font-size: 10px;
+}
+
+label {
+    color: $font-gray;
+    font-size: 16px;
 }
 
 </style>
