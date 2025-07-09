745a0adaf4ec4f7a3476d9437435a072ac03f1d8
dass90 <3053034939@qq.com>
2025-07-10 00:59:14 +0800

    [refactor] 将所有玩家的基本操作放在一个文件中

--- src/main/modules/player/basic.ts
+++ src/main/modules/player/basic.ts
@@ -1,9 +1,45 @@
-import { itemManager } from "../store";
+/**
+ * 玩家在游戏中所有的最最基本的且逻辑不复杂的操作
+ * 参与活动由于涉及到大量运算，因此不算最基本的操作
+ * 【如何判断是否是基本操作】
+ *  1. 这个基本操作需要涉及到1~2个复杂的需要因此封装的步骤
+ *  2. 不存在复杂的逻辑运算
+ * 
+ * 目前包括但不限于以下操作，若有遗漏，请添加。
+ * 商品系列：1. 购买商品 2. 使用物品
+ * 卡片系列：1. 获得卡片
+ */
+
 import { NotEnoughError, NotFoundError } from "../../error";
-import { playerManager } from "../store";
-import { PetMate } from "../petmate/petmate";
+import { Item } from "../../types/item";
 import { PlayerInfo } from "../../types/player";
+import { itemManager, playerManager } from "../store"
+import { PetMate } from "../petmate/petmate";
 import { Buff } from "../../types/buff";
+
+/**
+ * 购买物品
+ * Buff的CashConsumesRate不会影响到商品的价格
+ * @param itemId 物品id
+ * @param count 购买数量
+ * @throws 如果物品不存在则抛出NotFoundError
+ */
+export function buyItem(itemId: number, count: number): void {
+    const player:PlayerInfo = playerManager.getPlayer();
+    const playerItemNum: number = player.items.get(itemId) ?? 0;
+    const item:Item | undefined = itemManager.getItem(itemId);
+    if (!item) {
+        throw new NotFoundError(`购买物品的时候发现物品不存在: ${itemId}`);
+    }
+    const totalPrice = item.price * count;
+    const playerCash = player.cash;
+    if (playerCash < totalPrice) {
+        throw new NotEnoughError(`购买物品的时候发现玩家现金不足: 购买${count}个物品id[${itemId}]， 需要${totalPrice}元， 但是只有${playerCash}元`);
+    }
+    player.cash -= totalPrice;
+    player.items.set(itemId, playerItemNum + count);
+    playerManager.updatePlayer(player);
+}
 
 /**
  * 使用物品
