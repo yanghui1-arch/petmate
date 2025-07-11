b56d7303398d7a654f590ba0676db29a7a260144
gy <73433957+279795511445@users.noreply.github.com>
2025-07-11 18:12:22 +0800

    [feat] 新增活动页面 (#30)
    [chroe] 引入animation.css样式库
    [remove] 点滴路由

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'animate.css'
 import './assets/style/global.scss'
 import './assets/style/fonts.scss'
 createApp(App)
