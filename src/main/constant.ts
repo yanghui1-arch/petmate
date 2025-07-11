972378663f286c8363dedf4b344a4120641e33b6
dass90 <3053034939@qq.com>
2025-07-11 12:44:52 +0800

    [fix] 无法加载新数据的问题 (#27)

--- src/main/constant.ts
+++ src/main/constant.ts
@@ -1,3 +1,5 @@
+export const VERSION = "0.5.0";
+
 export const GET_BUFF_PROB_THROUGH_ACT = 0.2; // 通过活动获取buff的概率
 export const GET_BUFF_NUM_THROUGH_ACT = 2; // 通过活动获取buff的数量
 export const RETRY_TIMES_GET_BUFF_THROUGH_ACT = 3; // 重试获取buff的次数
