a8bd9de17f2936f65223f1d1575f38c957de1a7e
dass90 <3053034939@qq.com>
2025-07-20 01:13:54 +0800

    [feat] 展示模型配置信息

--- src/renderer/hooks/useShow.ts
+++ src/renderer/hooks/useShow.ts
@@ -1,3 +1,8 @@
+/**
+ * 所有跟页面展示的方法都放在这个hook下面
+ */
+
+import { ChatLLMConfig, TTSLLMConfig } from "../types/llm"
 import { ActivityInfo, Item, ItemType, Wish } from "../types/common"
 
 export function useShow() {
@@ -81,11 +86,40 @@ export function useShow() {
         }
     }
 
+    /**
+     * 获取Chat LLM和TTS LLM的配置
+     * @returns Chat LLM和TTS LLM的配置
+     */
+    const getLLMConfig = async (): Promise<{
+        chatLLMConfig: ChatLLMConfig | undefined,
+        ttsLLMConfig: TTSLLMConfig | undefined
+    }> => {
+        try {
+            const chatLLMConfigRes = await window.api.getChatLLMConfig()
+            const ttsLLMConfigRes = await window.api.getTTSLLMConfig()
+            if (chatLLMConfigRes.code === 200 && ttsLLMConfigRes.code === 200) {
+                return {
+                    chatLLMConfig: chatLLMConfigRes.data,
+                    ttsLLMConfig: ttsLLMConfigRes.data
+                }
+            } else {
+                throw new Error(chatLLMConfigRes.message || ttsLLMConfigRes.message)
+            }
+        } catch (error) {
+            console.error(error)
+            return {
+                chatLLMConfig: undefined,
+                ttsLLMConfig: undefined
+            }
+        }
+    }
+
 
     return {
         getPetmateCompletedWishesNum,
         getShopItems,
         getActivities,
-        getPetmateOneWish
+        getPetmateOneWish,
+        getLLMConfig
     }
 }
