8bedb7b4e5556bbe9e6b54d9ff329d8af3038276
dass90 <3053034939@qq.com>
2025-08-07 12:34:23 +0800

    [fix] 切回webhitstory模式

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 // 导入你的页面组件
 import Home from '@/views/Home.vue'
@@ -26,7 +26,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
